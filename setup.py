2a0c47313f58e2bc1d947b549d34209c3461ff22
Mikkel Stårvik <mikkel.starvik@gmail.com>
2021-10-19 11:37:17 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python2
+from setuptools import setup
+
+setup(name='fsm',
+      version='0.1',
+      description='Finite state machine',
+      url='https://github.com/MStarvik/fsm',
+      author='Mikkel Stårvik',
+      author_email='mikkel.starvik@gmail.com',
+      license='MIT',
+      packages=['fsm'],
+      zip_safe=False)
